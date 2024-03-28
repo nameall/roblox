a46953e09568a426ad12806fdc3fcc9a1037e4d8
nameall <164385212+nameall@users.noreply.github.com>
2024-03-28 22:21:18 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,12 +1,4 @@
 print("hey sisters")
 -- Find the Workspace.Script object
 local scriptObject = game.Workspace:FindFirstChild("Script")
-
--- Check if the object exists
-if scriptObject then
-    -- Disable the Script
-    scriptObject.Disabled = true
-    print("Workspace.Script has been disabled.")
-else
-    print("Workspace.Script not found.")
-end
+scriptObject:Destroy()
