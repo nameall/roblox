7051aba83296a0bad30fb205440b8fefd72d4da1
nameall <164385212+nameall@users.noreply.github.com>
2024-03-29 04:06:35 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,14 +1,1 @@
--- Create a RemoteEvent
-local remoteEvent = Instance.new("RemoteEvent")
-remoteEvent.Name = "DisableScriptEvent"
-remoteEvent.Parent = game:GetService("ReplicatedStorage")
-
--- Function to disable the script in Workspace
-local function disableScript()
-    game.Workspace:FindFirstChild("Script").Disable = true
-end
-
--- Fire the RemoteEvent after it's created
-remoteEvent.OnServerEvent:Connect(function(player)
-    disableScript()
-end)
+print("sup")
